e4e8734ef40b6fc003680108954fd8dedfc841c1
Artem Roma <aroma@mirantis.com>
2016-12-16 17:42:34 +0200

    Refactor app's code

--- agent/agent.go
+++ agent/agent.go
@@ -3,29 +3,21 @@ package agent
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"net"
 	"net/http"
 	"net/url"
-	"os"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/golang/glog"
-	//"github.com/vishvananda/netlink"
 )
 
-// NetcheckerAgentsEndpoint is part of URI providing access to
-// netchecker server that designates endpoint for retrieving
-// and updating of the agents
-var NetcheckerAgentsEndpoint = "/api/v1/agents"
+const NetcheckerAgentsEndpoint = "/api/v1/agents"
 
-// EnvVarPodName is name of environment variable that stores k8s pod name
-// inside which the agent is running
-var EnvVarPodName = "MY_POD_NAME"
+type Client interface {
+	Do(req *http.Request) (*http.Response, error)
+}
 
-// Payload describes request data to be sent to netchecker server
 type Payload struct {
 	ReportInterval string              `json:"report_interval"`
 	PodName        string              `json:"podname"`
@@ -34,92 +26,40 @@ type Payload struct {
 	IPs            map[string][]string `json:"ips"`
 }
 
-// Client represents HTTP client interface
-type Client interface {
-	Do(req *http.Request) (*http.Response, error)
-}
+func SendInfo(serverEndpoint, reportInterval, podName string, client Client) (*http.Response, error) {
+	reqURL := (&url.URL{
+		Scheme: "http",
+		Host:   serverEndpoint,
+		Path:   strings.Join([]string{NetcheckerAgentsEndpoint, podName}, "/"),
+	}).String()
 
-// SendMarshaled marshals given payload, constructs http request and
-// sends to the server URL
-func SendMarshaled(c Client, p *Payload, requestURL string) (*http.Response, error) {
-	glog.V(10).Infof("Request payload before marshaling: %v", p)
-	m, err := json.Marshal(p)
+	payload := &Payload{
+		HostDate:       time.Now(),
+		IPs:            linkV4Info(),
+		ReportInterval: reportInterval,
+		PodName:        podName,
+	}
+	addrs, err := net.LookupHost(strings.Split(serverEndpoint, ":")[0])
+	if err != nil {
+		glog.Errorf("DNS look up host error. Details: %v", err)
+	}
+	payload.LookupHost = addrs
+
+	glog.V(10).Infof("Request payload before marshaling: %v", payload)
+	marshaled, err := json.Marshal(payload)
 	if err != nil {
 		return nil, err
 	}
 
-	glog.V(5).Infof("Send payload via URL: %v", requestURL)
-	req, err := http.NewRequest("POST", requestURL, bytes.NewReader(m))
+	glog.V(5).Infof("Send payload via URL: %v", reqURL)
+	req, err := http.NewRequest("POST", reqURL, bytes.NewReader(marshaled))
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Add("Content-Type", "application/json")
 
-	resp, err := c.Do(req)
+	resp, err := client.Do(req)
 	return resp, err
-}
-
-// AnalyzeResponse is a place holder for more sophisticated response analysis
-func AnalyzeResponse(resp *http.Response) error {
-	if resp.StatusCode != 200 {
-		return errors.New("Response from the server is not OK")
-	}
-	return nil
-}
-
-// StartSending constructs and sends requests to the server in infinite loop
-func StartSending(serverEndpoint, reportInterval string) error {
-	var podName string
-	if podName = os.Getenv(EnvVarPodName); len(podName) == 0 {
-		return errors.New("Environment variable MY_POD_NAME is not set")
-	}
-
-	P := &Payload{
-		ReportInterval: reportInterval,
-		PodName:        podName,
-	}
-
-	reqURL := url.URL{
-		Scheme: "http",
-		Host:   serverEndpoint,
-		Path:   strings.Join([]string{NetcheckerAgentsEndpoint, podName}, "/"),
-	}
-	client := &http.Client{}
-
-	for {
-		// periodically flush cache to log file because app is run as
-		// daemon
-		glog.Flush()
-
-		P.HostDate = time.Now()
-
-		addrs, err := net.LookupHost(strings.Split(serverEndpoint, ":")[0])
-		if err != nil {
-			glog.Errorf("DNS look up host error. Details: %v", err)
-		}
-		P.LookupHost = addrs
-
-		P.IPs = linkV4Info()
-
-		resp, err := SendMarshaled(client, P, reqURL.String())
-		if err != nil {
-			return err
-		}
-
-		// let's just treat unsuccessful response as a warning
-		err = AnalyzeResponse(resp)
-		if err != nil {
-			glog.Warning("Analyzing response fails. Detals: %v", err)
-		}
-
-		sleepSeconds, err := strconv.Atoi(reportInterval)
-		if err != nil {
-			return err
-		}
-
-		glog.V(4).Infof("Sleep for %v second(s)", sleepSeconds)
-		time.Sleep(time.Duration(sleepSeconds) * time.Second)
-	}
 }
 
 func linkV4Info() map[string][]string {
--- cmd/app.go
+++ cmd/app.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"flag"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/aateem/mcp-netchecker-agent/agent"
 	"github.com/golang/glog"
 )
+
+const EnvVarPodName = "MY_POD_NAME"
 
 func main() {
 	var (
@@ -23,9 +28,28 @@ func main() {
 
 	glog.Info("Starting agent")
 
-	err := agent.StartSending(serverEndpoint, reportInterval)
-	if err != nil {
-		glog.Errorf("Cancel sending due to error. Details: %v", err)
+	var podName string
+	if podName = os.Getenv(EnvVarPodName); len(podName) == 0 {
+		glog.Error("Environment variable MY_POD_NAME is not set. No point in sending info. Exiting")
 		os.Exit(1)
+	}
+
+	sleepSeconds, err := strconv.Atoi(reportInterval)
+	if err != nil {
+		glog.Errorf("Error while processing report interval. Details: %v", err)
+		os.Exit(1)
+	}
+
+	client := &http.Client{}
+	for {
+		glog.V(4).Infof("Sleep for %v second(s)", sleepSeconds)
+		time.Sleep(time.Duration(sleepSeconds) * time.Second)
+
+		resp, err := agent.SendInfo(serverEndpoint, reportInterval, podName, client)
+		if err != nil {
+			glog.Errorf("Error while sending info. Details: %v", err)
+		} else {
+			glog.Infof("Response status code: %v", resp.StatusCode)
+		}
 	}
 }
